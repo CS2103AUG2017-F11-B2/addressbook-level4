fc48a5793b8b1bb141d0a5cab5a1d6249d9fd1c5
jiasheng <jiashengan@gmail.com>
2017-10-13 16:27:07 +0800

    Style change

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -18,13 +18,13 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
-        try {
+         try {
             Index index = ParserUtil.parseIndex(args);
             return new DeleteCommand(index);
-        } catch (IllegalValueException ive) {
+         } catch (IllegalValueException ive) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
-        }
+         }
     }
 
 }
