018f0303143456ef8d70f431671bcabd2d197d8e
jiasheng <jiashengan@gmail.com>
2017-10-13 16:35:48 +0800

    style change

--- src/main/java/seedu/address/logic/parser/DeleteTagCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteTagCommandParser.java
@@ -11,6 +11,7 @@ import seedu.address.model.tag.Tag;
  * Parses input arguments and creates a deleteTagCommand object.
  */
 public class DeleteTagCommandParser implements Parser<DeleteTagCommand> {
+    
     /**
      * Parses the given {@code String} of arguments in the context of the DeleteTagCommand
      * and returns an DeleteTagCommand object for execution.
