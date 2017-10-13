31474b33986a62ba85c16e55806674d78a93400b
jiasheng <jiashengan@gmail.com>
2017-10-13 17:28:19 +0800

    style change

--- src/main/java/seedu/address/logic/parser/DeleteTagCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteTagCommandParser.java
@@ -18,12 +18,12 @@ public class DeleteTagCommandParser implements Parser<DeleteTagCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteTagCommand parse(String args) throws ParseException {
-         try {
+        try {
             Tag targetTag = new Tag(args);
             return new DeleteTagCommand(targetTag);
-         } catch (IllegalValueException ab) {
+        } catch (IllegalValueException ab) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagCommand.MESSAGE_USAGE));
-         }
+        }
     }
 }
