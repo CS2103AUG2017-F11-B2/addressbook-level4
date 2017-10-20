538976d81dd2ca53c185bc3d3b0010ea13481ab3
jiasheng <jiashengan@gmail.com>
2017-10-20 18:03:09 +0800

    Junit test for ListOfWebLinkByCategory method in ReadOnlyPerson

---
+++ src/test/java/seedu/address/model/person/ListOfWebLinkByCategoryTest.java
@@ -0,0 +1,45 @@
+package seedu.address.model.person;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_WEB_LINK_DEFAULT;
+import static seedu.address.model.weblink.WebLinkUtil.FACEBOOK_TAG;
+import static seedu.address.testutil.TypicalPersons.AMY;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Set;
+
+import org.junit.Test;
+
+import seedu.address.model.weblink.WebLink;
+
+public class ListOfWebLinkByCategoryTest {
+
+    @Test
+    public void isValidOutputOfWebLinks(){
+        ReadOnlyPerson toAdd = AMY;
+        ArrayList<String> ExpectedOutput= new ArrayList<String>();
+        ExpectedOutput.add(VALID_WEB_LINK_DEFAULT);
+        assertTrue(isTwoArrayListsWithSameValues(ExpectedOutput, AMY.listOfWebLinkByCategory(FACEBOOK_TAG)));
+    }
+
+    public boolean isTwoArrayListsWithSameValues(ArrayList<String> list1, ArrayList<String> list2)
+    {
+        //null checking
+        if(list1==null && list2==null)
+            return true;
+        if((list1 == null && list2 != null) || (list1 != null && list2 == null))
+            return false;
+
+        if(list1.size()!=list2.size())
+            return false;
+        for(Object itemList1: list1)
+        {
+            if(!list2.contains(itemList1))
+                return false;
+        }
+
+        return true;
+    }
+}
