715ad4171567200fa73a3f69fab3da4bb3d6fe8c
Han Siang Phua <hansiang93@gmail.com>
2017-10-30 20:29:42 +0800

    Added autocomplete messages for all commands

--- src/main/java/seedu/address/ui/AutoCompleteSuggestions.java
+++ src/main/java/seedu/address/ui/AutoCompleteSuggestions.java
@@ -21,27 +21,41 @@ import seedu.address.logic.commands.UndoCommand;
 import seedu.address.logic.commands.UpdateUserCommand;
 import seedu.address.logic.commands.WebCommand;
 
+/**
+ * Handles all command suggestions and their usage examples.
+ */
 public class AutoCompleteSuggestions {
     private static ArrayList<String> suggestionList = new ArrayList<>();
     static {
         suggestionList.add(AddCommand.COMMAND_WORD);
+        suggestionList.add(AddCommand.MESSAGE_USAGE_EXAMPLE);
         suggestionList.add(EditCommand.COMMAND_WORD);
+        suggestionList.add(EditCommand.MESSAGE_USAGE_EXAMPLE);
         suggestionList.add(SelectCommand.COMMAND_WORD);
+        suggestionList.add(SelectCommand.MESSAGE_USAGE_EXAMPLE);
         suggestionList.add(DeleteCommand.COMMAND_WORD);
+        suggestionList.add(DeleteCommand.MESSAGE_USAGE_EXAMPLE);
         suggestionList.add(ClearCommand.COMMAND_WORD);
         suggestionList.add(FindCommand.COMMAND_WORD);
+        suggestionList.add(FindCommand.MESSAGE_USAGE_EXAMPLE);
         suggestionList.add(FilterCommand.COMMAND_WORD);
+        suggestionList.add(FilterCommand.MESSAGE_USAGE_EXAMPLE);
         suggestionList.add(ListCommand.COMMAND_WORD);
         suggestionList.add(SortCommand.COMMAND_WORD);
+        suggestionList.add(SortCommand.MESSAGE_USAGE_EXAMPLE);
         suggestionList.add(HistoryCommand.COMMAND_WORD);
         suggestionList.add(ExitCommand.COMMAND_WORD);
         suggestionList.add(HelpCommand.COMMAND_WORD);
         suggestionList.add(UndoCommand.COMMAND_WORD);
         suggestionList.add(RedoCommand.COMMAND_WORD);
         suggestionList.add(DeleteTagCommand.COMMAND_WORD);
+        suggestionList.add(DeleteTagCommand.MESSAGE_USAGE_EXAMPLE);
         suggestionList.add(RemarkCommand.COMMAND_WORD);
+        suggestionList.add(RemarkCommand.MESSAGE_USAGE_EXAMPLE);
         suggestionList.add(UpdateUserCommand.COMMAND_WORD);
+        suggestionList.add(UpdateUserCommand.MESSAGE_USAGE_EXAMPLE);
         suggestionList.add(WebCommand.COMMAND_WORD);
+        suggestionList.add(WebCommand.MESSAGE_USAGE_EXAMPLE);
     }
     
     public static ArrayList<String> getSuggestionList() {
